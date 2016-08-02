fa2d7cc3a0996c0c4873c464d931cf6180a40e64
lombardo-chcg <dpa.lombardo@gmail.com>
2016-08-02 13:57:47 -0500

    Add index route's ability to render a collection

--- src/main/java/com/lombardo/giflib/controller/GifController.java
+++ src/main/java/com/lombardo/giflib/controller/GifController.java
@@ -5,10 +5,12 @@ import com.lombardo.giflib.model.Gif;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Controller
 public class GifController {
@@ -16,13 +18,15 @@ public class GifController {
     private GifRepo gifRepo;
 
     @RequestMapping(value = "/")
-    public String listGifs() {
+    public String listGifs(ModelMap modelMap) {
+        List<Gif> allGifs = gifRepo.getAllGifs();
+        modelMap.put("gifs", allGifs);
         return "home";
     }
 
-    @RequestMapping(value = "/gif")
-    public String gifDetails(ModelMap modelMap) {
-        Gif gif = gifRepo.findByName("android-explosion");
+    @RequestMapping(value = "/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap) {
+        Gif gif = gifRepo.findByName(name);
         modelMap.put("gif", gif);
         return "gif-details";
     }
